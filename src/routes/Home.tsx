d3a8e7a1ac90e786d5493ea414f84c1c2c24d9aa
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-03-05 10:14:20 -0400

    I used fetch to use the github api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { userProps } from "../types/user";
+import Search from "./../components/Search";
+
+import { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [user, setUser] = useState<userProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+    </div>
+  );
 };
 
 export default Home;
